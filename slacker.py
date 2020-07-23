b773498af7ffb1ec58c9e9231553f842dfed9baa
Letzter, Evan <Evan_Letzter@comcast.com>
2020-07-23 19:51:06 -0400

    Discontinue subprocess and use threading instead

--- slacker.py
+++ slacker.py
@@ -1,18 +1,40 @@
 import pyautogui as auto
 import PySimpleGUI as sg
 import time
-import subprocess
+import threading
+import ctypes
 
 def move_mouse():
     '''
     Move the mouse back and forth. Click after every other move.
     '''
     while True:
-        auto.moveRel(100, 0, duration=0.2)
-        time.sleep(5)
         auto.moveRel(-100, 0, duration=0.2)
         auto.click()
-        time.sleep(5)
+        time.sleep(3)
+        auto.moveRel(100, 0, duration=0.2)
+        time.sleep(3)
+
+class thread_with_exception(threading.Thread): 
+    def __init__(self, name, target, daemon): 
+        threading.Thread.__init__(self, target=target, daemon=daemon) 
+        self.name = name
+           
+    def get_id(self): 
+        # returns id of the respective thread 
+        if hasattr(self, '_thread_id'): 
+            return self._thread_id 
+        for id, thread in threading._active.items(): 
+            if thread is self: 
+                return id
+   
+    def raise_exception(self): 
+        thread_id = self.get_id() 
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 
+              ctypes.py_object(SystemExit)) 
+        if res > 1: 
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0) 
+            print('Exception raise failure') 
 
 def interface():
     '''
@@ -24,25 +46,26 @@ def interface():
     layout = [ [sg.Text('Status:'), sg.Text('Stopped', key='-STATUS-', text_color='red'),
                sg.Button('Start', key='-START-'), sg.Button('Stop', key='-STOP-')] ]
 
-    window = sg.Window('Green Check', layout, element_justification='center', size=(250,40))
+    window = sg.Window('Slacker', layout, element_justification='center', size=(250,40))
 
     while True:
-        event, values = window.read()
+        event, _ = window.read(timeout=10)
+
+        if event is None:
+            break
 
         if event == '-START-' and window['-STATUS-'].get() == 'Stopped':
-            window['-STATUS-'].update('Running', text_color='green')
-            sp = subprocess.Popen(["python", '-c', 'from green_check import move_mouse; move_mouse()'])
+            window['-STATUS-'].update('Slacking', text_color='green')
+            thread_id = thread_with_exception('Thread 1', target=move_mouse, daemon=False)
+            thread_id.start()
+            print(f'{thread_id} started')
         
-        if event == '-STOP-' and window['-STATUS-'].get() == 'Running':
+        if event == '-STOP-' and window['-STATUS-'].get() == 'Slacking':
             window['-STATUS-'].update('Stopped', text_color='red')
-            sp.kill()
-        
-        if event is None:
-            try:
-                sp.kill()
-            except NameError:
-                pass
-            break
+            thread_id.raise_exception()
+            thread_id.join()
+            print(f'{thread_id} killed')
+            
     
     window.close()
 
